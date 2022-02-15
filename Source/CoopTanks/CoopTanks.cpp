215f336b0aa80bc01954344fd89edaad39733d7f
Dominik Tekiela <65705492+iLuzj0@users.noreply.github.com>
2022-02-15 11:46:35 +0100

    Change naming for enemies components

---
+++ Source/CoopTanks/CoopTanks.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoopTanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoopTanks, "CoopTanks" );
